101a551064f7641a9332a38f412a793f12bc6bf9
pverly0 <pedro.verly@ufv.br>
2022-05-05 15:35:17 -0300

    TableUp

--- src/Styles/styles.js
+++ src/Styles/styles.js
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
         flex: 1, 
         justifyContent: 'center',
         alignItems: 'center',
-        
+        padding: 4
     },
     logo: {
         width: 350,
