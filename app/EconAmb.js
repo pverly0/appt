1309a67a57f4ee10b4c5e568f380ce87f9b9a384
pverly0 <pedro.verly@ufv.br>
2022-05-04 21:13:51 -0300

    X

--- app/EconAmb.js
+++ app/EconAmb.js
@@ -19,7 +19,7 @@ function Econ(props) {
             <Text style={{fontSize:30, color:'white', top: 5, alignSelf: 'center'}}>Insira os dados:</Text>
             
             <View>
-                <Text style={{fontSize:20, color:'white', textAlign:'left', top:20, marginVertical:12,}}>Volume</Text>
+                <Text style={{fontSize:20, color:'white', textAlign:'left', top:20, marginVertical:12,}}>Valor X</Text>
                 <TextInput
                     style={{color:'white', fontSize:20,
                     borderWidth:1, borderColor:'white', height:30, width:170, marginVertical:12,
