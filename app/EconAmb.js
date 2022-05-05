a0a4fea0cfd410091c801578283f139917737f88
pverly0 <pedro.verly@ufv.br>
2022-05-04 21:09:33 -0300

    UpdateCaixa

--- app/EconAmb.js
+++ app/EconAmb.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { ImageBackground, Image,
     Text, StatusBar,
-    ScrollView } from 'react-native';
+    View, TextInput } from 'react-native';
 import styles from 'appt/src/Styles/styles';
-import CaixaValor from 'appt/src/Styles/CaixaValor';
 
 <styles/>;
 
@@ -19,13 +18,15 @@ function Econ(props) {
             
             <Text style={{fontSize:30, color:'white', top: 5, alignSelf: 'center'}}>Insira os dados:</Text>
             
-            <CaixaValor/>
-            <CaixaValor/>
-            <CaixaValor/>
-            <CaixaValor/>
-            <CaixaValor/>
-            <CaixaValor/>
-            <CaixaValor/>
+            <View>
+                <Text style={{fontSize:20, color:'white', textAlign:'left', top:20, marginVertical:12,}}>Volume</Text>
+                <TextInput
+                    style={{color:'white', fontSize:20,
+                    borderWidth:1, borderColor:'white', height:30, width:170, marginVertical:12,
+                    position: 'absolute', right:1, top:20}}
+                    keyboardType='decimal-pad'
+                />
+            </View>
             
         </ImageBackground>
     );
